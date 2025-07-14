5a1a59640d08cbc3404d5e3efed8a1703b80f650
AErgueta <tu-emailexample.com>
2025-07-13 23:07:40 -0400

    Iniciamos con la publicación

--- src/database.js
+++ src/database.js
@@ -1,10 +1,12 @@
+require('dotenv').config(); // ⬅️ Esto carga el archivo .env
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://aerguetab:analgesico@cluster0.2tvoknd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {    
-    // ---- Esto se quitó del código, porque la versión de la Base es diferente ---- //
-    //useCreateIndex: true,
-    //useNewUrlParse: true, 
-    //useFindAndModify: false 
+// Usar la variable desde el archivo .env
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
-    .then(db => console.log('DB is connected')) //Cuando te conectes muestra en consola DB is connected
-    .catch(err => console.error(err)); // PROMESA
+    .then(() => console.log('DB is connected'))
+    .catch(err => console.error('DB connection error:', err));
